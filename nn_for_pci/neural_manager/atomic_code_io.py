78ecf34a3193db5ff5e3baf827a21ddc5985cd59
Pavlo Bilous <pavlo.bilous@mpl.mpg.de>
2025-05-24 17:28:54 +0200

    Docstrings to methods of AtomicCodeIO added.

--- nn_for_pci/neural_manager/atomic_code_io.py
+++ nn_for_pci/neural_manager/atomic_code_io.py
@@ -10,24 +10,33 @@ class AtomicCodeIO(ABC):
 
     @abstractmethod
     def read_full_basis(self) -> Numpy2DArray:
+        """Takes no input and returns a 2D NumPy array for the full CI basis set with rows corresponding
+        to relativistic configurations and columns representing relativistic orbital populations."""
         pass
 
 
     @abstractmethod
     def read_prior_basis(self) -> Numpy2DArray:
+        """Takes no input and returns a 2D NumPy array for the "prior" CI basis set with rows corresponding
+        to relativistic configurations and columns representing relativistic orbital populations."""
         pass
 
 
     @abstractmethod
     def read_prior_weights(self) -> Numpy1DArray[float]:
+        """Takes no input and returns a 1D NumPy array with weights obtained in the "prior" computation."""
         pass
 
 
     @abstractmethod
     def read_current_weights(self) -> Numpy1DArray[float]:
+        """Takes no input and returns a 1D NumPy array with weights obtained in the "current" CI run."""
         pass
 
 
     @abstractmethod
     def write_current_basis(self, which_write: Numpy1DArray[bool]) -> None:
+        """Writes input for the next CI run.
+        The argument which_write is a 1D boolean NumPy array with True
+        corresponding to the relativistic configurations to be written."""
         pass
